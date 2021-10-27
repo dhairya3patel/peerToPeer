44b1ace7064fc7b80cc5f2e56c92335682795a9e
Tanishq Shaikh <tanishq.s@somaiya.edu>
2021-10-27 17:22:27 -0400

    fingerTable

--- project3.fsx
+++ project3.fsx
@@ -2,13 +2,14 @@
 #r "nuget: Akka.Remote"
 
 open System
+open System.Collections.Generic
+open System.Collections.Specialized;
 open Akka.Actor
 open Akka.FSharp
 open System.Security.Cryptography
 open System.Text
 
 let mutable numNodes = fsi.CommandLineArgs.[1] |> int64
-let mutable ring = []
 // let requests = fsi.CommandLineArgs.[2]
 let system = ActorSystem.Create("Project3")
 
@@ -16,11 +17,17 @@ type Communication =
     | Start of string
     | BuildFingerTable of string
     | Initiate of String //list <IActorRef>
+    | Join of string
+    | Temp of string * IActorRef
 
-let nodes = numNodes |> float
-let m = Math.Round(Math.Log(nodes, 2.)) |> int
+// let nodes = numNodes |> float
+// let m = Math.Ceiling(Math.Log(nodes, 2.)) |> int
 
-Console.WriteLine(m)
+// let mutable ring = []
+// let dummy = spawn system "dummy"
+// let mutable ring = Array.create (pown 2 m) null
+
+// Console.WriteLine(m)
 
 let sha1Hash input: string =
     let sha = new SHA1Managed()
@@ -32,17 +39,19 @@ let sha1Hash input: string =
         |> String.concat String.Empty
     hashS
 
-let mutable seed = "Peer_"
+// let mutable seed = "Peer_"
 
-for i in 1 .. 17 do
-    seed <- seed + i.ToString()
-    let ans = sha1Hash seed
-    let position = ans.[0..m]
-    let decValue = Convert.ToInt64(position, 16)
-    Console.WriteLine(decValue)
+// for i in 1 .. 17 do
+//     seed <- seed + i.ToString()
+//     let ans = sha1Hash seed
+//     let position = ans.[0..m]
+//     let decValue = Convert.ToInt64(position, 16)
+//     Console.WriteLine(decValue)
+// let buildFingerTable input: string =
 
-
+        
 let peer (mailbox: Actor<_>) =
+    let fingerTable = OrderedDictionary()
     let mutable predecessor = null
     let mutable successor = null
 
@@ -53,15 +62,26 @@ let peer (mailbox: Actor<_>) =
             match peermessage with
             | Initiate(_) ->
                 successor <- mailbox.Self
-                Console.WriteLine("Ring created")
-                let hashedValue = sha1Hash mailbox.Self
-                Console.WriteLine("Hash: " + hashedValue)
-                let position = hashedValue.[0..m - 1]
-                let decValue = Convert.ToInt64(position, 16)
-                let ringPosition = (decValue |> int) % pown 2 m
-                ring <- ringPosition :: ring
-                Console.WriteLine(ring)
-
+                let selfAddress = mailbox.Self.Path.Name.Split('_').[1] |> int
+                fingerTable.Add(selfAddress + 1, selfAddress)
+                Console.WriteLine(fingerTable.[0])
+                // Console.WriteLine("Ring created")
+                // let hashedValue = sha1Hash mailbox.Self.Path.Name.Split("_")
+                // Console.WriteLine("Hash: " + hashedValue)
+                // let position = hashedValue.[hashedValue.Length - m.. 40]
+                // Console.WriteLine("Position: " + position)
+                // let decValue = Convert.ToInt64(position, 16)
+                // Console.WriteLine("Dec value: " + decValue.ToString())
+                // let temp = decValue |> uint
+                // Console.WriteLine("Debug" + temp.ToString())
+                // let a = 2 |> uint
+                // let b = pown a m |> uint
+                // let ringPosition = (decValue |> uint) % b |> int
+                // ring <- ringPosition :: ring
+                // ring.[ringPosition] <- mailbox.Self.Path.Name
+                // mailbox.Sender() <! Temp(hashedValue, mailbox.Self)
+                // Array.set ring ringPosition mailbox.Self
+                // Console.WriteLine("Ring " + (Array.get ring ringPosition).ToString())
 
             | _ -> ignore()
 
@@ -73,7 +93,7 @@ let peer (mailbox: Actor<_>) =
 let master (mailbox: Actor<_>) =
     let mutable peersList = []
     let numNodes = numNodes |> int
-
+    // let mutable ring = Array.create (pown 2 m) null
     let rec loop() =
         actor {
             let! message = mailbox.Receive()
@@ -82,9 +102,19 @@ let master (mailbox: Actor<_>) =
                 peersList <-
                     [ for i in 1 .. numNodes do
                         yield (spawn system ("Peer_" + string (i))) peer ]
-                Console.WriteLine(peersList.ToString())
-                peersList.[0] <! Initiate("Begin")
+                
+                // Console.WriteLine(peersList.ToString())
+                // peersList.[0] <! Initiate("Begin")
 
+                Console.WriteLine("Ring initiated")
+                for x in 0 .. 4 do 
+                    peersList.[x] <! Initiate("Begin")
+
+            | Temp(hashedValue, selfAddress) -> Console.WriteLine("Hashed Value: " + hashedValue)
+                                                // Array.set ring hashedValue selfAddress
+                                                //  Console.WriteLine("Position " + ringPosition.ToString() + " " + (Array.get ring ringPosition).ToString())
+                                                //  for y in ring do
+                                                //     Console.WriteLine(Array.get ring y)
 
             | _ -> ignore()
 
@@ -95,3 +125,4 @@ let master (mailbox: Actor<_>) =
 let masterActor = spawn system "master" master
 
 masterActor <! Start("Start")
+system.WhenTerminated.Wait()
